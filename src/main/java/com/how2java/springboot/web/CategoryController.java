23e274145fc8e7492ee5ea7069b76b68d15deef3
yslu <469899431@qq.com>
2019-01-21 18:02:38 +0800

    springboot

--- src/main/java/com/how2java/springboot/web/CategoryController.java
+++ src/main/java/com/how2java/springboot/web/CategoryController.java
@@ -2,11 +2,11 @@ package com.how2java.springboot.web;
 
 import com.how2java.springboot.mapper.CategoryMapper;
 import com.how2java.springboot.pojo.Category;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -16,15 +16,13 @@ import java.util.List;
  */
 @Controller
 public class CategoryController {
-    @Autowired
+    @Resource
     CategoryMapper categoryMapper;
 
     @RequestMapping("/listCategory")
     public String listCategory(Model m) throws Exception {
-        List<Category> cs=categoryMapper.findAll();
-
+        List<Category> cs = categoryMapper.findAll();
         m.addAttribute("cs", cs);
-
         return "listCategory";
     }
 }
