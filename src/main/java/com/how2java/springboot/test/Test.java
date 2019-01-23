4900dd94ebdba26218a3e2d1d32d2d6704a6a53f
yslu <469899431@qq.com>
2019-01-23 09:40:02 +0800

    springboot

---
+++ src/main/java/com/how2java/springboot/test/Test.java
@@ -0,0 +1,39 @@
+package com.how2java.springboot.test;
+
+
+import com.how2java.springboot.SpringbootApplication;
+import com.how2java.springboot.mapper.CategoryMapper;
+import com.how2java.springboot.pojo.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+//引入配置的bean工厂（已经注入过很多bean了）
+@SpringBootTest(classes = SpringbootApplication.class)
+public class Test {
+
+//    @Resource  资源引入
+
+    //自动装配（从工厂中取出）
+    @Autowired
+    CategoryMapper categoryMapper;
+
+//    @Before
+//    public void begore(){
+//        context = new ClassPathXmlApplicationContext("classpath:spring/spring-config.xml");
+//        categoryMapper = (CategoryMapper)context.getBean("categoryMapper");
+//    }
+
+    @org.junit.Test
+    public void test2() {
+        List<Category> cs = categoryMapper.findAll();
+        for (Category c : cs) {
+            System.out.println("c.getName():" + c.getName());
+        }
+
+    }
+}
